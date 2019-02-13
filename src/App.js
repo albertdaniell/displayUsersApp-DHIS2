0cfa2e28d617c93aa70359ae164a9943f59a7d3e
alby <albertagoya@gmail.com>
2019-02-13 08:28:04 +0300

    First commt

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        const fetchOptions = {
+            headers: {
+                Authorization: `Basic ${btoa('admin:district')}`
+            }
+        };
+        fetch('https://test.hiskenya.org/kenya/api/users', fetchOptions)
+        //Parse the result as json format
+            .then(res => res.json())
+            .then((result) => {
+
+                this.setState({isLoading: false, mydata: result.users})
+
+                console.log(this.state.mydata)
+            }, (error) => {
+                console.log(error)
+            });
+
+    }
+
+    state = {
+        isLoading: true,
+        loadingText: 'Loading...',
+        mydata: []
+
+    }
+
+    render() {
+        // Set the 'Authorization' header to authenticate with the DHIS instance
+
+        let loading = this.state.isLoading
+            ? this.state.loadingText
+            : 'Users Results found!!!'
+        return (
+            <div className="container">
+                <h1 className='text-center'>React Js DHIS2 Api</h1>
+                <hr/>
+
+                <div>
+
+                    <p className="text-center">
+                        <span className="badge badge-primary">{loading}</span>
+                    </p>
+                    {this
+                        .state
+                        .mydata
+                        .map(data => (
+                            <li key='data.id'>
+                                {data.displayName}
+
+                            </li>
+                        ))
+}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
